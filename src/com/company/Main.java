44aa03bd3f4e2759cbaeea34cca0f35874f6ce8a
LszQ0 <michallech94@gmail.com>
2023-05-01 17:22:46 +0200

    Interview question: display numbers from 1-100. It's "buzz" when it's divisible by 3, "fuzz" if it's divisible by 5. "fuzzbuzz" when it's divisible by both numbers
    +changed the layout of the code to be easier to read

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,25 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-        for(int i = 1; i<=100; i++)
-        {
-            if (i % 15 == 0)
-            {
+        for(int i = 1; i < 101; i++) {
+            if (i % 15 == 0) {
                 System.out.println("fuzzbuzz");
-
-                }
-            else if (i % 5 == 0)
-            {
+            } else if (i % 5 == 0) {
                 System.out.println("fuzz");
-            }
-            else if (i % 3 == 0)
-            {
+            } else if (i % 3 == 0) {
                 System.out.println("buzz");
-            }
-            else {
+            } else {
                 System.out.println(i);
             }
-
         }
         System.out.println("Thank you for checking my program");
 
